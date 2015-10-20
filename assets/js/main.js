a73195e1f9a53136c1bbe2c07c7a32da029c2c33
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-20 19:18:30 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// alert
+// $(document).ready(function(){
+// 	$("button").click(function(){
+// 		alert("Background color = " + $("p").css("background-color"))
+// });
+// });
+
+//styling with css using jquery, usually used for styling something(eg pop window). so even if ur js doesnt work, u can still do wo it
+$(document).ready(function(){
+	$("p").css({
+ 		"background-color": "green",
+ 		"color": "red",
+ 	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
